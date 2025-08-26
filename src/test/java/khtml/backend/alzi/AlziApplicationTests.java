38a61267332f0cf74f6d185b3ce721e7f3cc313d
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-08-26 22:51:49 +0900

    First Commit : 기초세팅

---
+++ src/test/java/khtml/backend/alzi/AlziApplicationTests.java
@@ -0,0 +1,13 @@
+package khtml.backend.alzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
